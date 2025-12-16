585c8ad704586a75fcd33a8120ee4582e8858107
Fernando João Manfroi <fernando.manfroi@sea.sc.gov.br>
2025-12-16 17:54:42 -0300

    agendamento

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 
 app = FastAPI()
+
+# Configura a pasta onde estão os arquivos HTML
+templates = Jinja2Templates(directory="templates")
 
 # Submodelo para o campo "mensagem"
 class Mensagem(BaseModel):
@@ -13,9 +18,13 @@ class DadosCliente(BaseModel):
     origem: str
     mensagem: Mensagem
 
-@app.get("/")
-def healthz():
-    return {"status": "Backend online"}
+class Agendamento(BaseModel):
+    hora: str
+    minuto: str
+
+@app.get("/", response_class=HTMLResponse)
+def ler_pagina(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/api/registrar")
 def receber_dados(dados: DadosCliente):
@@ -28,4 +37,16 @@ def receber_dados(dados: DadosCliente):
         "recebido": True,
         "origem": dados.origem,
         "evento": dados.mensagem.evento
+    }
+
+# Rota que recebe os dados do agendamento via JSON
+@app.post("/api/agendar")
+def agendar_horario(dados: Agendamento):
+    print(f"Recebido agendamento para: {dados.hora}:{dados.minuto}")
+    
+    # AQUI entra a lógica (ex: salvar no banco de dados ou agendar o job)
+    
+    return {
+        "status": "sucesso", 
+        "mensagem": f"Tarefa agendada para as {dados.hora}:{dados.minuto}"
     }
